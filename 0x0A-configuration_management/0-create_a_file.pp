8340514ba3860f1cf43d14e99e21b09ca4c66d8f
amir-kedis <amir.kedis@gmail.com>
2024-04-21 10:39:32 +0200

    0x0A Configuration Management - Task 0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# Ensure the file /tmp/school is present
+file { '/tmp/school':
+  ensure  => file,
+  content => 'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+}
